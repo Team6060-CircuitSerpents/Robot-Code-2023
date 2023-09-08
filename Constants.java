a2f24da5338ed56450b2c3a755d841b960d42dd3
Circuit Serpents <126981007+Team6060-CircuitSerpents@users.noreply.github.com>
2023-09-08 16:18:36 -0700

    Constants.java

--- Constants.java
+++ Constants.java
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// THIS INCLUDES BOTH MOTOR IDS AND ONE ENCODER ID FOR EACH WHEEL INCLUDING THE STEER OFFSET 
 
 package frc.robot;
 
